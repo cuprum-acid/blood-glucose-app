843ce02ae5bfe21c50c8ec6642e86db3e2c75eb2
anyar <anyarylova51@gmail.com>
2023-07-01 01:09:46 +0300

    🐛💄 Started to work with navigation

--- app/src/main/java/com/example/bloodglucose/MainActivity.kt
+++ app/src/main/java/com/example/bloodglucose/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.bloodglucose
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.android_large___1)
+        setContentView(R.layout.start)
+        val button: Button = findViewById(R.id.button_to_profile_screen)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/bloodglucose/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bloodglucose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+
+    }
+}
