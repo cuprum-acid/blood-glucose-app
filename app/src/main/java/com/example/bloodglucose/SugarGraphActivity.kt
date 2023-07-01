976b1543031bd4a75755cf0c84871de970f1d423
anyar <anyarylova51@gmail.com>
2023-07-01 07:04:57 +0300

    🐛💄 Fixed profile.xml, advice.xml, today.xml, sugar_level_graph.xml screens, added connections

--- app/src/main/java/com/example/bloodglucose/SugarGraphActivity.kt
+++ app/src/main/java/com/example/bloodglucose/SugarGraphActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bloodglucose
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SugarGraphActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sugar_level_graph)
+
+        val backButton: Button = findViewById(R.id.button_graph_back)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this@SugarGraphActivity, ProfileActivity::class.java)
+            startActivity(backIntent)
+        }
+
+        val exportButton: Button = findViewById(R.id.button_graph_export)
+
     }
 }
--- app/src/main/java/com/example/bloodglucose/TodayActivity.kt
+++ app/src/main/java/com/example/bloodglucose/TodayActivity.kt
@@ -1,6 +1,5 @@
 package com.example.bloodglucose
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,5 +9,17 @@ class TodayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.today)
+
+        val homeButton: Button = findViewById(R.id.button_home_today)
+        homeButton.setOnClickListener {
+            val homeIntent = Intent(this@TodayActivity, ProfileActivity::class.java)
+            startActivity(homeIntent)
+        }
+
+        val adviceButton: Button = findViewById(R.id.button_today_advice)
+        adviceButton.setOnClickListener {
+            val adviceIntent = Intent(this@TodayActivity, AdviceActivity::class.java)
+            startActivity(adviceIntent)
+        }
     }
 }
