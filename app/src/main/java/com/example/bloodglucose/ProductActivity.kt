1a5289b691feee9dc699a92b68b71d0cc3d4e941
anyar <anyarylova51@gmail.com>
2023-07-03 01:12:45 +0300

    🐛 Fixed product_list.xml

--- app/src/main/java/com/example/bloodglucose/ProductActivity.kt
+++ app/src/main/java/com/example/bloodglucose/ProductActivity.kt
@@ -9,6 +9,13 @@ import android.widget.Button
 class ProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.list_pr)
+        setContentView(R.layout.product_list)
+
+        val backButton: Button = findViewById(R.id.button_back_add_pr)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this@ProductActivity, ProfileActivity::class.java)
+            startActivity(backIntent)
+        }
+
     }
 }
