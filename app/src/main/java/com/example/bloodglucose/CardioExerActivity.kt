3f00d04dca89fe476755536d77b3fa45692fa6f6
Julia <you@example.com>
2023-07-07 23:54:42 +0300

    Finished: StaticExerActivity, PowerExerActivity, CardioExerActivity All activities are represented on User Interface 😉

--- app/src/main/java/com/example/bloodglucose/CardioExerActivity.kt
+++ app/src/main/java/com/example/bloodglucose/CardioExerActivity.kt
@@ -3,7 +3,12 @@ package com.example.bloodglucose
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class CardioExerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +20,50 @@ class CardioExerActivity : AppCompatActivity() {
             val backIntent = Intent(this@CardioExerActivity, ListExercisesActivity::class.java)
             startActivity(backIntent)
         }
+        val cardioEx = ArrayList <String> ()
+        Firebase.firestore.collection("exercises").whereEqualTo("category", "cardio").get().
+                addOnSuccessListener { exercises ->
+                    for (exercise in exercises) {
+                        val item = exercise.id
+                        cardioEx.add(item)
+                    }
+                    val listView: ListView = findViewById(R.id.listView)
+                    val arrayAdapter = ArrayAdapter(
+                        this, android.R.layout.simple_list_item_1,
+                        cardioEx
+                    )
+                    listView.adapter = arrayAdapter
+
+
+                    // Set the item click listener
+                    listView.setOnItemClickListener { parent, view, position, id ->
+                        // Get the selected item
+                        val selectedItem = parent.getItemAtPosition(position) as String
+
+                        // save the chosen item and go back to the previous screen:
+
+                        val exerciseCollection =
+                            Firebase.firestore.collection("users").document(USER_ID)
+                                .collection("takenExercises")
+
+                        // create a hashmap of values to be uploaded to the database
+                        val product = hashMapOf(
+                            "datetime" to FieldValue.serverTimestamp(),
+                            "exerciseId" to selectedItem
+                        )
+
+                        exerciseCollection.add(product)
+
+
+                        val backIntent =
+                            Intent(this@CardioExerActivity, ProductActivity::class.java)
+                        startActivity(backIntent)
+                    }
+                }
+            .addOnFailureListener{ exception ->
+                println("Error getting documents: $exception")
+
+        }
+
     }
 }
--- app/src/main/java/com/example/bloodglucose/StaticExerActivity.kt
+++ app/src/main/java/com/example/bloodglucose/StaticExerActivity.kt
@@ -3,7 +3,12 @@ package com.example.bloodglucose
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class StaticExerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +20,49 @@ class StaticExerActivity : AppCompatActivity() {
             val backIntent = Intent(this@StaticExerActivity, ListExercisesActivity::class.java)
             startActivity(backIntent)
         }
+        val staticEx = ArrayList <String> ()
+        Firebase.firestore.collection("exercises").whereEqualTo("category", "static").get().
+        addOnSuccessListener { exercises ->
+            for (exercise in exercises) {
+                val item = exercise.id
+                staticEx.add(item)
+            }
+            val listView: ListView = findViewById(R.id.listView)
+            val arrayAdapter = ArrayAdapter(
+                this, android.R.layout.simple_list_item_1,
+                staticEx
+            )
+            listView.adapter = arrayAdapter
+
+
+            // Set the item click listener
+            listView.setOnItemClickListener { parent, view, position, id ->
+                // Get the selected item
+                val selectedItem = parent.getItemAtPosition(position) as String
+
+                // save the chosen item and go back to the previous screen:
+
+                val exerciseCollection =
+                    Firebase.firestore.collection("users").document(USER_ID)
+                        .collection("takenExercises")
+
+                // create a hashmap of values to be uploaded to the database
+                val product = hashMapOf(
+                    "datetime" to FieldValue.serverTimestamp(),
+                    "exerciseId" to selectedItem
+                )
+
+                exerciseCollection.add(product)
+
+
+                val backIntent =
+                    Intent(this@StaticExerActivity, ProductActivity::class.java)
+                startActivity(backIntent)
+            }
+        }
+            .addOnFailureListener{ exception ->
+                println("Error getting documents: $exception")
+
+            }
     }
 }
