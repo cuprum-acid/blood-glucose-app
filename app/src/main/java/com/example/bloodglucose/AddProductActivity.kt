bc596706039d0fe9bc12f967b3d98e68a7b404d0
VikaLa7 <7322vipgmail.com>
2023-07-13 16:43:32 +0300

    :fire: Spinners on Add activities are implemented

--- app/src/main/java/com/example/bloodglucose/AddProductActivity.kt
+++ app/src/main/java/com/example/bloodglucose/AddProductActivity.kt
@@ -3,7 +3,9 @@ package com.example.bloodglucose
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 
 class AddProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,18 @@ class AddProductActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             val backIntent = Intent(this@AddProductActivity, ProductActivity::class.java)
             startActivity(backIntent)
+        }
+
+        val spinner: Spinner = findViewById(R.id.spinner_add_products)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.add_pr,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
         }
     }
 }
